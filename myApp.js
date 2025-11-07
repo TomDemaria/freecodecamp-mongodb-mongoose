d28edd46e4aefd4b6d83dbf02e898d4c889f908f
TomDemaria <112651624+TomDemaria@users.noreply.github.com>
2025-11-07 18:20:23 +1000

    Update and rename myApp to myApp.js

--- myApp.js
+++ myApp.js
@@ -8,11 +8,11 @@ mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
-.then(() => console.log('✅ Conectado a MongoDB Atlas'))
-.catch(err => console.error('❌ Error al conectar con MongoDB:', err));
+.then(() => console.log(' Conectado a MongoDB Atlas'))
+.catch(err => console.error(' Error al conectar con MongoDB:', err));
 
 app.get('/', (req, res) => {
-  res.send('Servidor y base de datos configurados correctamente 🚀');
+  res.send('Servidor y base de datos configurados correctamente ');
 });
 
 const PORT = process.env.PORT || 3000;
